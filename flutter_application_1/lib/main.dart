187dd8dd2a3bdf80706ff77128d5112f99f6eaf5
AnettVera <20223tn034@utez.edu.mx>
2025-09-21 20:51:53 -0600

    [ADD] clase persona y estudiante, formulario para agregar un objeto

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Student.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,27 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -37,15 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -58,62 +35,95 @@ class _MyHomePageState extends State<MyHomePage> {
   int age = 21;
   String name = "Mali";
   bool programing = true;
+  final List<String> students = ['Alumno1', 'Alumno2', 'Alumno3'];
+  final Student student = Student("Mali", "20223tn034");
+  TextEditingController _txtName = TextEditingController();
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   void _decrementCounter() {
     setState(() {
-      if (_counter > 0){
+      if (_counter > 0) {
         _counter--;
       }
-      
     });
   }
 
+  Widget _getAllStudents() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 12),
+        Text(
+          "Students:",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 12),
+        ...students.map((n) => Text("- $n")).toList(),
+      ],
+    );
+  }
+
+//funcion nueva para agregar un objeto de estudiante
+
+  TextEditingController _txtNombre = TextEditingController();
+  TextEditingController _txtId = TextEditingController();
+  final List<Student> studentList = [];
+void _addStudentObject(){
+  final nombre= _txtNombre.text.trim();
+  final matricula= _txtId.text.trim();
+  if(nombre.isEmpty || matricula.isEmpty){
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Por favor llene todos los campos")));
+    return;
+  }
+  setState((){
+    studentList.add(Student(nombre, matricula));
+  });
+  _txtNombre.clear();
+  _txtId.clear();
+}
+Widget _getObjectStudents(){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children:[
+      const SizedBox(height: 12),
+      Text("Lista de las instancias del objeto estudiante", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+      SizedBox(height: 12),
+      ...studentList.map((studiante)=> Text("- ${studiante.name}, ${studiante.studentId}")).toList(),
+    ]
+  );
+}
+
+void _addStudent(){
+  final name = _txtName.text.trim();
+  if(name.isEmpty){
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("Please write something"))
+    );
+    return;
+
+  }
+  setState(() {
+    students.add(name);
+  });
+  _txtName.clear();
+}
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+   
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
@@ -121,9 +131,62 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12),
+              child: TextField(
+                controller: _txtNombre,
+                decoration: InputDecoration(
+                  labelText: "Nombre",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12),
+              child: TextField(
+                controller: _txtId,
+                decoration: InputDecoration(
+                  labelText: "Matricula",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12),
+              child: ElevatedButton(
+                onPressed: _addStudentObject,
+                child: Text("Add Student"),
+              )
+             
+            ),
+             Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12),
+              child: TextField(
+                controller: _txtName,
+                decoration: InputDecoration(
+                  labelText: "Name",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12),
+              child: ElevatedButton(
+                onPressed: _addStudent,
+                child: Text("Add Student"),
+              )
+             
+            ),
+
+            SizedBox(height: 15),
             Text('Nombre : $name'),
             Text('Edad: $age'),
             Text('Programo?: $programing'),
+            SizedBox(height: 15),
+            Text("Student1: ${student.name} "),
+            _getObjectStudents(),
+            _getAllStudents(),
           ],
         ),
       ),
@@ -136,7 +199,7 @@ class _MyHomePageState extends State<MyHomePage> {
             tooltip: 'Increment',
             child: const Icon(Icons.add),
           ),
-          const SizedBox(width: 16), 
+          const SizedBox(width: 16),
           FloatingActionButton(
             onPressed: _decrementCounter,
             tooltip: 'Decremente',
@@ -145,7 +208,6 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
 
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
